1ef6fc7f19268780b7b0fedf145511ebb4fe66a7
chieh0666 <jerry970246@gmail.com>
2025-02-13 22:45:06 +0800

    新增網頁預載入功能及統一搜尋框樣式，調整公告訊息字體大小

---
+++ js/component.js
@@ -0,0 +1,16 @@
+// 網頁預載入js
+$(document).ready(function(){
+  $('.search-input').focus(function(){
+    $('.search-form').removeClass('border-secondary-subtle').addClass('border-primary');
+    $('.search-btn').removeClass('border-secondary-subtle').addClass('border-primary');
+  })
+  $('.search-input').blur(function(){
+    $('.search-form').removeClass('border-primary').addClass('border-secondary-subtle');
+    $('.search-btn').removeClass('border-primary').addClass('border-secondary-subtle');
+  })
+});
+
+// 首頁論播圖
+const container = document.getElementById("myCarousel");
+const options = { infinite: true, Autoplay: {timeout: 3000} };
+new Carousel(container, options, { Autoplay });
